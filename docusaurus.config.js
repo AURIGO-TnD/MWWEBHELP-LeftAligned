d1d790f045be3b6691087511e9ded19f35fe0ce8
SohiniC <127090119+SohiniC@users.noreply.github.com>
2023-05-17 15:44:27 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/Modules/MWWHELP/en-US',
+  baseUrl: '/Modules/MWWHELP/fr-CA',
   onBrokenLinks: 'throw',
   favicon: 'img/Favicon.png',
   organizationName: 'your-org', // Usually your GitHub org/user name.
