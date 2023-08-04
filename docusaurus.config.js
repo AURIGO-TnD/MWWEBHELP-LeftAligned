19807b20ef5667938ac278107d6dc44f3122cf33
Shankar-Aurigo <141415723+Shankar-Aurigo@users.noreply.github.com>
2023-08-04 18:35:15 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/Modules/MWWHELP/en-GB/',
+  baseUrl: '/Modules/MWWHELP/en-US/',
   onBrokenLinks: 'throw',
   favicon: 'img/Favicon.png',
   organizationName: 'your-org', // Usually your GitHub org/user name.
